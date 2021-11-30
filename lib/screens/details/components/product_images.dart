8589eeb1cbeebb883ccc3f262e8e97c6f6d57e0e
Binit <binitkoirala17@gmail.com>
2021-11-30 13:12:16 +0545

    added changes from open source project

---
+++ lib/screens/details/components/product_images.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import '/models/Product.dart';
+
+import '/utils/constants.dart';
+import '/utils/size_config.dart';
+
+class ProductImages extends StatefulWidget {
+  const ProductImages({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  _ProductImagesState createState() => _ProductImagesState();
+}
+
+class _ProductImagesState extends State<ProductImages> {
+  int selectedImage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: SizeConfig.heightMultiplier * 28,
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Hero(
+              tag: widget.product.id.toString(),
+              child: Image.asset(widget.product.images[selectedImage]),
+            ),
+          ),
+        ),
+        // SizedBox(height: getProportionateScreenWidth(20)),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ...List.generate(widget.product.images.length,
+                (index) => buildSmallProductPreview(index)),
+          ],
+        )
+      ],
+    );
+  }
+
+  GestureDetector buildSmallProductPreview(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedImage = index;
+        });
+      },
+      child: AnimatedContainer(
+        duration: defaultDuration,
+        margin: EdgeInsets.only(
+          right: SizeConfig.heightMultiplier * 2,
+        ),
+        padding: EdgeInsets.all(SizeConfig.heightMultiplier),
+        height: SizeConfig.heightMultiplier * 6,
+        width: SizeConfig.heightMultiplier * 6,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+              color: kPrimaryColor.withOpacity(selectedImage == index ? 1 : 0)),
+        ),
+        child: Image.asset(widget.product.images[index]),
+      ),
+    );
+  }
+}
