3d7a61e29824a1a17bd511300609e15907475290
Binit <binitkoirala17@gmail.com>
2021-12-01 15:00:51 +0545

    changed components to body

--- lib/screens/otp/otp_screen.dart
+++ lib/screens/otp/otp_screen.dart
@@ -1,16 +1,179 @@
+import '/components/default_button.dart';
+import '/constant/color_properties.dart';
+import '/utils/size_config.dart';
 import 'package:flutter/material.dart';
 
-import 'components/body.dart';
-
 class OtpScreen extends StatelessWidget {
-  static String routeName = "/otp";
+  static const String routeName = "/otp";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("OTP Verification"),
+        title: const Text("OTP Verification"),
       ),
-      body: Body(),
+      body: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: SizeConfig.heightMultiplier * 2.5,
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.heightMultiplier),
+                const Text(
+                  "OTP Verification",
+                ),
+                const Text("We sent your code to +1 898 860 ***"),
+                buildTimer(),
+                const OtpForm(),
+                SizedBox(height: SizeConfig.heightMultiplier),
+                GestureDetector(
+                  onTap: () {
+                    // OTP code resend
+                  },
+                  child: const Text(
+                    "Resend OTP Code",
+                    style: TextStyle(decoration: TextDecoration.underline),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row buildTimer() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("This code will expired in "),
+        TweenAnimationBuilder(
+          tween: Tween(begin: 30.0, end: 0.0),
+          duration: const Duration(seconds: 30),
+          builder: (_, dynamic value, child) => Text(
+            "00:${value.toInt()}",
+            style: const TextStyle(color: primaryColor),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class OtpForm extends StatefulWidget {
+  const OtpForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _OtpFormState createState() => _OtpFormState();
+}
+
+class _OtpFormState extends State<OtpForm> {
+  FocusNode? pin2FocusNode;
+  FocusNode? pin3FocusNode;
+  FocusNode? pin4FocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    pin2FocusNode = FocusNode();
+    pin3FocusNode = FocusNode();
+    pin4FocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pin2FocusNode!.dispose();
+    pin3FocusNode!.dispose();
+    pin4FocusNode!.dispose();
+  }
+
+  void nextField(String value, FocusNode? focusNode) {
+    if (value.length == 1) {
+      focusNode!.requestFocus();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          SizedBox(height: SizeConfig.heightMultiplier * .5),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: SizeConfig.heightMultiplier * 7.5,
+                child: TextFormField(
+                  autofocus: true,
+                  obscureText: true,
+                  // style: TextStyle(fontSize: 24),
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+
+                  onChanged: (value) {
+                    nextField(value, pin2FocusNode);
+                  },
+                ),
+              ),
+              SizedBox(
+                width: SizeConfig.heightMultiplier * 7.5,
+                child: TextFormField(
+                  focusNode: pin2FocusNode,
+                  obscureText: true,
+                  // style: TextStyle(fontSize: 24),
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+
+                  onChanged: (value) => nextField(value, pin3FocusNode),
+                ),
+              ),
+              SizedBox(
+                width: SizeConfig.heightMultiplier * 7.5,
+                child: TextFormField(
+                  focusNode: pin3FocusNode,
+                  obscureText: true,
+                  // style: TextStyle(fontSize: 24),
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+
+                  onChanged: (value) => nextField(value, pin4FocusNode),
+                ),
+              ),
+              SizedBox(
+                width: SizeConfig.heightMultiplier * 7.5,
+                child: TextFormField(
+                  focusNode: pin4FocusNode,
+                  obscureText: true,
+                  // style: TextStyle(fontSize: 24),
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+
+                  onChanged: (value) {
+                    if (value.length == 1) {
+                      pin4FocusNode!.unfocus();
+                      // Then you need to check is the code is correct or not
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: SizeConfig.heightMultiplier,
+          ),
+          DefaultButton(
+            text: "Continue",
+            press: () {},
+          )
+        ],
+      ),
     );
   }
 }
--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,17 +1,140 @@
+import '/constant/color_properties.dart';
+import '/utils/size_config.dart';
 import 'package:flutter/material.dart';
-
-
-import 'components/body.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ProfileScreen extends StatelessWidget {
-  static String routeName = "/profile";
+  static const String routeName = "/profile";
+
+  const ProfileScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Profile"),
+        title: const Text("Profile"),
       ),
-      body: Body(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: Column(
+          children: [
+            const ProfilePic(),
+            const SizedBox(height: 20),
+            ProfileMenu(
+              text: "My Account",
+              icon: "assets/icons/User Icon.svg",
+              press: () => {},
+            ),
+            ProfileMenu(
+              text: "Notifications",
+              icon: "assets/icons/Bell.svg",
+              press: () {},
+            ),
+            ProfileMenu(
+              text: "Settings",
+              icon: "assets/icons/Settings.svg",
+              press: () {},
+            ),
+            ProfileMenu(
+              text: "Help Center",
+              icon: "assets/icons/Question mark.svg",
+              press: () {},
+            ),
+            ProfileMenu(
+              text: "Log Out",
+              icon: "assets/icons/Log out.svg",
+              press: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileMenu extends StatelessWidget {
+  const ProfileMenu({
+    Key? key,
+    required this.text,
+    required this.icon,
+    this.press,
+  }) : super(key: key);
+
+  final String text, icon;
+  final VoidCallback? press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: SizeConfig.heightMultiplier * 2.5,
+        vertical: SizeConfig.heightMultiplier * 1.3,
+      ),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.all(SizeConfig.heightMultiplier * 2.5),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              SizeConfig.heightMultiplier * 2,
+            ),
+          ),
+        ),
+        onPressed: press,
+        child: Row(
+          children: [
+            SvgPicture.asset(
+              icon,
+              color: primaryColor,
+              // width: 22,
+            ),
+            SizedBox(width: SizeConfig.heightMultiplier * 2.5),
+            Expanded(child: Text(text)),
+            const Icon(Icons.arrow_forward_ios),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfilePic extends StatelessWidget {
+  const ProfilePic({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 115,
+      width: 115,
+      child: Stack(
+        fit: StackFit.expand,
+        clipBehavior: Clip.none,
+        children: [
+          const CircleAvatar(
+            backgroundImage: AssetImage("assets/images/Profile Image.png"),
+          ),
+          Positioned(
+            right: -16,
+            bottom: 0,
+            child: SizedBox(
+              height: 46,
+              width: 46,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    side: const BorderSide(color: colorWhite),
+                  ),
+                  primary: Colors.white,
+                  backgroundColor: colorWhite,
+                ),
+                onPressed: () {},
+                child: SvgPicture.asset("assets/icons/Camera Icon.svg"),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
